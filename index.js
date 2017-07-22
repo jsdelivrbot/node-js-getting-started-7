00e8c95b9f41cf1203ad8e6a999bf408679d610c
inspectaTech <d3po.techengineer@gmail.com>
2017-07-22 08:32:52 -0400

    test 004

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 var server = require('ws').Server;
 
-//var s = new server({port:5001});
-var s = new WebSocketServer({port:80});
+var s = new server({port:5001});
+//var s = new WebSocketServer({port:80});
 //var s = new WebSocket('ws://cryptic-ocean-42125.herokuapp.com');
 
 
