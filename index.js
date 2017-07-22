49609ed0135a53ec49d942efba9a07f95f3e6a91
inspectaTech <d3po.techengineer@gmail.com>
2017-07-22 12:31:20 -0400

    t020

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ s.on('connection',function(ws){
 });
 */
 
+/*
 //websocket example
 //#!/usr/bin/env node
 var WebSocketServer = require('websocket').server;
@@ -99,7 +100,35 @@ wsServer.on('request', function(request) {
             connection.sendBytes(message.binaryData);
         }
     });
+
     connection.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
+*/
+//heroku's example
+//'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send(new Date().toTimeString());
+  });
+}, 1000);
